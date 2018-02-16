970cbd602f23260973be869ea344f220110a5fb4
Neville <live.neville@live.com>
2018-02-15 23:07:30 -0500

    Manual validate extensions cleaned and fixed recursion

--- CustomValidationExtensions.cs
+++ CustomValidationExtensions.cs
@@ -10,52 +10,96 @@ namespace NetCore.Simple
     public static class CustomValidationExtensions
     {
 
-        //static IActionResult ValidatedResult(this Controller Controller, object model, Func<IActionResult> Success,
-        //                                                        IEnumerable<ErrorModel> additionalErrors,
-        //                                                        Func<List<ErrorModel>> manualValidate)
-        //{
-        //    Controller.ModelState.Clear();
-        //    Controller.TryValidateModel(model);
-        //    if (Controller.ModelState.IsValid)
-        //    {
-        //        return Success();
-        //    }
-        //    return Controller.BadRequest(Controller.ModelState.Errors(additionalErrors));
-        //}
-
-        public static IActionResult ValidatedResult(this Controller Controller, object model, Action OnSuccess,
-                                                                IEnumerable<ErrorModel> additionalErrors = null,
-                                                                Func<List<ErrorModel>> manualValidate = null)
-            => Controller.ValidatedResult(model, () => {
-                OnSuccess();
-                return Controller.Ok();
-            }, additionalErrors, manualValidate);
-
-        public static IActionResult ValidatedResult(this Controller Controller, object model, Func<object> OnSuccess,
-                                                            IEnumerable<ErrorModel> additionalErrors = null,
-                                                            Func<List<ErrorModel>> manualValidate = null)
-            => Controller.ValidatedResult(model, () => Controller.Ok(OnSuccess()), additionalErrors, manualValidate);
-
-        public static IActionResult ValidatedResult(this Controller Controller, object model, 
-                                                                IEnumerable<ErrorModel> additionalErrors = null,
-                                                                Func<List<ErrorModel>> manualValidate = null)
-            => Controller.ValidatedResult(model, () => Controller.Ok(), additionalErrors, manualValidate);
-
-
-        public static async Task<IActionResult> ValidatedResult(this Controller Controller, object model, 
-                                    Func<Task<object>> OnSuccess, IEnumerable<ErrorModel> additionalErrors = null,
-                                    Func<List<ErrorModel>> manualValidate = null)
+        static bool Revalidate(this Controller controller, object model)
         {
-            Controller.ModelState.Clear();
-            Controller.TryValidateModel(model);
-            if (Controller.ModelState.IsValid)
-            {
-                var obj = await OnSuccess();
-                return Controller.Ok(obj);
-            }
-            return Controller.BadRequest(Controller.ModelState.Errors(additionalErrors, manualValidate));
+            controller.ModelState.Clear();
+            controller.TryValidateModel(model);
+            return controller.ModelState.IsValid;
         }
         
+        /// <summary>
+        /// For void call back or no call back
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="OnSuccess">callback</param>
+        /// <param name="additionalErrors"></param>
+        /// <param name="manualValidate"></param>
+        /// <returns></returns>
+        public static IActionResult ValidatedResult(this Controller controller, object model, Action OnSuccess = null, 
+                                    IEnumerable<ErrorModel> additionalErrors = null,
+                                    Func<List<ErrorModel>> manualValidate = null)
+        {
+            if (controller.Revalidate(model))
+            {
+                OnSuccess?.Invoke();
+                return controller.Ok();
+            }
+            else return controller.BadRequest(controller.ModelState.Errors(additionalErrors, manualValidate));
+        }
+
+        /// <summary>
+        /// Call back to directly return the validation result
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="OnSuccess"></param>
+        /// <param name="additionalErrors"></param>
+        /// <param name="manualValidate"></param>
+        /// <returns></returns>
+        public static IActionResult ValidatedResult(this Controller controller, object model,
+                                    Func<IActionResult> OnSuccess, IEnumerable<ErrorModel> additionalErrors = null,
+                                    Func<List<ErrorModel>> manualValidate = null)
+        {
+
+            if (controller.Revalidate(model)) return OnSuccess();
+            return controller.BadRequest(controller.ModelState.Errors(additionalErrors, manualValidate));
+        }
+
+        /// <summary>
+        /// Call back to return any object (will return Ok(obj))
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="OnSuccess"></param>
+        /// <param name="additionalErrors"></param>
+        /// <param name="manualValidate"></param>
+        /// <returns></returns>
+        public static IActionResult ValidatedResult(this Controller controller, object model,
+                                    Func<object> OnSuccess, IEnumerable<ErrorModel> additionalErrors = null,
+                                    Func<List<ErrorModel>> manualValidate = null)
+                => controller.ValidatedResult(model, () => controller.Ok(OnSuccess()), additionalErrors, manualValidate);
+
+        /// <summary>
+        /// async call back with direct action result
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="OnSuccess"></param>
+        /// <param name="additionalErrors"></param>
+        /// <param name="manualValidate"></param>
+        /// <returns></returns>
+        public static async Task<IActionResult> ValidateResultAsync(this Controller controller, object model,
+                                    Func<Task<IActionResult>> OnSuccess, IEnumerable<ErrorModel> additionalErrors = null,
+                                    Func<List<ErrorModel>> manualValidate = null)
+        {
+            if (controller.Revalidate(model)) return await OnSuccess();
+            return controller.BadRequest(controller.ModelState.Errors(additionalErrors, manualValidate));
+        }
+
+        /// <summary>
+        /// Async call back with any object
+        /// </summary>
+        /// <param name="controller"></param>
+        /// <param name="model"></param>
+        /// <param name="OnSuccess"></param>
+        /// <param name="additionalErrors"></param>
+        /// <param name="manualValidate"></param>
+        /// <returns></returns>
+        public static async Task<IActionResult> ValidateResultAsync(this Controller controller, object model,
+                                    Func<Task<object>> OnSuccess, IEnumerable<ErrorModel> additionalErrors = null,
+                                    Func<List<ErrorModel>> manualValidate = null)
+            => await controller.ValidateResultAsync(model, async () => await OnSuccess(), additionalErrors, manualValidate);
 
 
     }
